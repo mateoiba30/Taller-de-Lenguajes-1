bd87948bbe19080a47722e3318f4ab0475343724
Mateo Ibanez <mateoiba30@gmail.com>
2023-04-05 09:02:59 -0300

    hago ej 2 y cambio nombres

---
+++ practica 3/p3-ej2-typedef.c
@@ -0,0 +1,22 @@
+// struct persona {
+//  char nombre[50];
+//  long int DNI;
+// } unaPersona;
+
+// typedef struct persona persona_t;//le cambio el nombre
+
+typedef struct persona {
+ char nombre[50];
+ long int DNI;
+} persona_t; //lo mismo pero abreviado
+
+
+int main(){
+
+    persona_t p1;
+    //unaPersona.nombre;
+
+    p1.nombre;
+
+    return 0;
+}
