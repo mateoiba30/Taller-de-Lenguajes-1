3e28aec03520af5bb8ab0d69b64e9d49e4e428e4
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-06 20:02:08 -0300

    inicializo y veo tablero

--- practica4-opcional/busca_minas.c
+++ practica4-opcional/busca_minas.c
@@ -4,6 +4,26 @@
 
 #define BOMBAS 10
 #define LADOS 8
+#define CHAR_BOMBA 229//EB
+#define CHAR_OCULTA 177//B1
+#define CHAR_BANDERA 16
+
+// i es fila
+//j es para columna
+//1 para verdadero
+
+typedef enum  {sup1, sup2, sup3, izq, der, pos1, pos2, pos3 } enumCasillasVecinas;
+// 0 1 2
+// 3 x 4
+// 5 6 7
+
+typedef struct{//HACERLOS CAMPOS DE BITS?
+    int tieneBandera;
+    int esBomba;
+    int bombasVecinas;
+    int esVisible;
+
+}casilla;
 
 typedef struct{
     int fila;
@@ -11,20 +31,94 @@ typedef struct{
 
 }punto2D;
 
+void impirmirBombasTablero(casilla[][LADOS]);
+void inicializarTablero(casilla[][LADOS], punto2D[]);
+int contarBombasVecinas(int, int, punto2D[]);
+void reconocerVecinos (int [], int, int);
+
+void imprimirPosBombas(punto2D[]);
 void elegirBombas(punto2D[], int *);
 int buscarPunto(punto2D, punto2D[], int);
-void imprimirPosBombas(punto2D[]);
 
 int main(){
 
-    srand(time(NULL));
-    int tablero[LADOS][LADOS], dimlBombas=-1;//diml es cuantos voy cargando
+    // srand(time(NULL));
+    casilla tablero[LADOS][LADOS];
+    int dimlBombas=-1;//diml es cuantos voy cargando
     punto2D posBombas[BOMBAS];
 
     elegirBombas(posBombas, &dimlBombas);
     // imprimirPosBombas(posBombas);
+    inicializarTablero(tablero, posBombas);
+    impirmirBombasTablero(tablero);
 
     return 0;
+}
+
+void impirmirBombasTablero(casilla tablero[][LADOS]){
+    for(int i=0; i<LADOS; i++){
+        for(int j=0; j<LADOS; j++){
+            if(tablero[i][j].esBomba)
+                printf("%c", CHAR_BOMBA);
+            else
+                printf("%c", CHAR_OCULTA);
+            printf("\t");
+        }
+        printf("\n \n");
+    }
+}
+
+void inicializarTablero(casilla tablero[][LADOS], punto2D posBombas[]){
+    for(int i=0; i<LADOS; i++){
+        for(int j=0; j<LADOS; j++){
+
+            punto2D nuevoPunto;
+            nuevoPunto.fila=i;
+            nuevoPunto.columna=j;
+            if(buscarPunto(nuevoPunto, posBombas, BOMBAS)==1)//1 si encuentra al punto como bomba
+                tablero[i][j].esBomba=1;
+            else
+                tablero[i][j].esBomba=0;
+
+            tablero[i][j].tieneBandera=0;
+            tablero[i][j].esVisible=0;
+            tablero[i][j].bombasVecinas=contarBombasVecinas(i,j,posBombas);
+            
+        }
+    }
+}
+
+int contarBombasVecinas(int i, int j, punto2D posBombas[]){
+    int contador=0, casillasVecinas[8]={1, 1, 1, 1, 1, 1, 1, 1};//si está con un 1 significa que tiene esa casilla
+    reconocerVecinos(casillasVecinas, i, j);
+    for(int k=0; k<8; k++){
+        if(casillasVecinas[k]==1)
+            contador++;
+    }
+    return contador;
+}
+
+void reconocerVecinos (int casillasVecinas[], int i, int j){
+    if(j ==0){//no tiene hermanos a la izquierda
+        casillasVecinas[sup1]=0;
+        casillasVecinas[izq]=0;
+        casillasVecinas[pos1]=0;
+    }
+    if(j == (LADOS - 1)){//no tiene hermanos a la derecha
+        casillasVecinas[sup3]=0;
+        casillasVecinas[der]=0;
+        casillasVecinas[pos3]=0;
+    }
+    if(i ==0){//no tiene hermanos a la arriba
+        casillasVecinas[sup1]=0;
+        casillasVecinas[sup2]=0;
+        casillasVecinas[sup3]=0;
+    }
+    if(j == (LADOS - 1)){//no tiene hermanos a abajo
+        casillasVecinas[pos1]=0;
+        casillasVecinas[pos2]=0;
+        casillasVecinas[pos3]=0;
+    }
 }
 
 void imprimirPosBombas(punto2D posBombas[]){
@@ -36,8 +130,8 @@ void imprimirPosBombas(punto2D posBombas[]){
 void elegirBombas(punto2D posBombas[], int *dimlBombas){//no poner bombas entre los corchetes porque lo tendría que poner tmb en el prototipo y es a fin de cuentas innecesario
     punto2D nuevoPunto;
     for(int i=0; i<=BOMBAS; i++){// <= para cargar todas las bombas
-        nuevoPunto.fila=rand()%(LADOS +1);
-        nuevoPunto.columna=rand()%(LADOS +1);
+        nuevoPunto.fila=rand()%(LADOS);
+        nuevoPunto.columna=rand()%(LADOS);
         if( buscarPunto(nuevoPunto, posBombas, (*dimlBombas)) == 0){
             (*dimlBombas)++;
             posBombas[(*dimlBombas)-1]=nuevoPunto;//cargo al final
