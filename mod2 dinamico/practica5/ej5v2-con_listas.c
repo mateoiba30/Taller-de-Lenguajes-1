482581dbf59b03b2e1a952888444fbeb58f2347a
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-17 11:02:55 -0300

    anda ej5v2 como pide la consigna

--- mod2 dinamico/practica5/ej5v2-con_listas.c
+++ mod2 dinamico/practica5/ej5v2-con_listas.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 struct nodo{//DEBO PONERLE NOMBRE AL INICIO
-    char *dato;
+    char dato[255];
     struct nodo* sig;
 };
 
 typedef struct nodo nodo;
 typedef nodo* lista;
 
+void imprimirLista(lista );
 void inicializarLista(lista*);
 void liberarLista(lista*);
-void agregarInicio(lista*, int );
+void agregarInicio(lista*, char* );
+void buscarPalabra(lista, char[], int*);
+
 
 int main(){
     FILE *f;
@@ -25,28 +28,31 @@ int main(){
     }
     fprintf(f,"mateo\njuan\npepe\n");
     fseek(f, 0, SEEK_SET);//suando escribo me mueve el puntero del archivo
-    
-    printf("Ingrese una palabra a buscar (ZZZ para finalizar): ");
-    scanf("%s", palabra);
-    if(strcmp(palabra,"ZZZ")==0)
-        fin=1;
 
     lista l;
     inicializarLista(&l);
 
-    while (!feof(f)){
-        fscanf(f, "%s", palabra_act);//mejor que usar fgets porque pone bien el \0
-        agregarInicio(&l,palabra_act);
-    }
+    // while (!feof(f)){//verifica si me pasé, entonces la última la lee 2 veces
+    //     fscanf(f, "%s", palabra_act);//mejor que usar fgets porque pone bien el \0
+    //     agregarInicio(&l,palabra_act);
+    // } //debería eliminar el elemento repetido
 
+    while (fscanf(f, "%s", palabra_act) == 1) {//Si un error de entrada ocurre antes de cualquier conversión, la función fscanf retorna EOf
+        agregarInicio(&l, palabra_act);
+    }
+    // imprimirLista(l); //pa debuguear
+
+    printf("Ingrese una palabra a buscar (ZZZ para finalizar): ");
+    scanf("%s", palabra);
+    if(strcmp(palabra,"ZZZ")==0)
+        fin=1;
     while(fin==0){
-        
+        buscarPalabra(l,palabra,&encontre);
         if(encontre==1)
             printf("palabra encontrada\n");
         else
             printf("palabra NO encontrada\n");
 
-        fseek(f, 0, SEEK_SET); // Volver al principio del archivo porque leer me mueve el puntero
         encontre=0;
         printf("Ingrese otra palabra a buscar (ZZZ para finalizar): ");
         scanf("%s", palabra);
@@ -58,19 +64,31 @@ int main(){
     else
         printf("palabra NO encontrada\n");
 
-    liberarLista(l);
+    liberarLista(&l);
     fclose(f);
     return 0;
 }
+void buscarPalabra(lista l, char palabra[], int* encontre){
+    lista act=l, aux, ant;//recorro con auxiliar para no perder el 1er puntero
+    while(act!=NULL && (*encontre)==0){
+        if(strcmp(palabra,act->dato)==0)
+            (*encontre)=1;
+        else{
+            ant=act;
+            act=act->sig;//sinó solo avanzo
+        }
+    }
+}
+
 
 void inicializarLista(lista* l){
     (*l)=NULL;
 }
 
-void agregarInicio(lista* l, int dato){
+void agregarInicio(lista* l, char dato[]){
     lista act;
     act=(lista)malloc(sizeof(nodo));//reservo mem
-    act->dato=dato;
+    strcpy(act->dato,dato);//destino, fuente
     act->sig=(*l);
     (*l)=act;
 
@@ -83,4 +101,14 @@ void liberarLista(lista* l){
         (*l)=(*l)->sig;
         free(aux);
     }
+}
+
+
+void imprimirLista(lista l){
+    lista aux=l;
+        while(aux!=NULL){
+            printf("%s, ",aux->dato);
+            aux=aux->sig;
+    }
+
 }
