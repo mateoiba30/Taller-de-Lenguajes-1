415549caa2d3b56aa6ad3890b379aeeeedafd549
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-26 14:19:19 -0300

    termino ej1

--- mod2 dinamico/practica5-adicional/ej1-difucultad_palabras.c
+++ mod2 dinamico/practica5-adicional/ej1-difucultad_palabras.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #define MAX 16
 #define ABC 26
+#define CANT_MAX 14
+#define CANT_MIN 4
 
 typedef struct{
     char palabra[MAX];
@@ -9,7 +11,7 @@ typedef struct{
 } Palabra;
 
 void cargarBinario(FILE*, FILE*);
-float obtenerDificultad(char [], int*);
+int analizarPalabra(char[], float *, int*);
 void imprimirArchivo(FILE *);
 
 int main(){
@@ -41,11 +43,10 @@ int main(){
 
 void cargarBinario(FILE*binario, FILE*texto){
     Palabra p;
-    int problema=0;
+    int cantLetras;
 
     while(fscanf(texto, "%s", p.palabra)!=EOF){//scanf no incluye el \n pero corta en espacios (no es un problema en las palabras)
-        p.dificultad=obtenerDificultad(p.palabra, &problema);
-        if(problema==0){
+        if(analizarPalabra(p.palabra, &p.dificultad, &cantLetras)==0 && cantLetras<= CANT_MAX && cantLetras >= CANT_MIN){
             // printf("%s\t%f\n", p.palabra, p.dificultad);
             fwrite(&p, sizeof(Palabra), 1, binario);
         }
@@ -53,28 +54,28 @@ void cargarBinario(FILE*binario, FILE*texto){
     fseek(binario, 0, SEEK_SET);
 }
 
-float obtenerDificultad(char palabra[], int *problema){
+int analizarPalabra(char palabra[], float * dificultad, int *cantLetras){
     float frecuencias[ABC]={12.53, 1.42, 4.68, 5.86, 13.68, 0.69, 1.01, 0.7, 6.25, 0.44, 0.01, 4.97, 3.15, 
                             6.71, 8.68, 2.51, 0.88, 6.87, 7.98, 4.63, 3.93, 0.9, 0.02, 0.22, 0.9, 0.52};
     char cAct, aux='a', aux2=aux-'A';
     int i=0;
-    float dificultad=0;
+    (*dificultad)=0;
 
-    (*problema)=0;
     cAct=palabra[i];
-    while(cAct!='\0' && (*problema)==0){
+    while(cAct!='\0'){
         if(cAct>='A' && cAct <= 'Z')
             cAct=cAct+aux2;
         // printf("%d ", cAct);
         if(cAct==-61)// reconoce error ante ñ o Ñ
-            (*problema)=1;
-        dificultad+=frecuencias[cAct-aux];
+            return 1;
+        (*dificultad)+=frecuencias[cAct-aux];
 
         i++;
         cAct=palabra[i];
     }
+    (*cantLetras)=i;
 
-    return dificultad;
+    return 0;
 }
 
 void imprimirArchivo(FILE *binario){
